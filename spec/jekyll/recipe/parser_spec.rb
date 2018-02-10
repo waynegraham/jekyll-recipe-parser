dc9cf9708745ac1da90b5ebb8e363a02aac5e2fc
Wayne Graham <wgraham@clir.org>
2018-02-10 15:25:17 -0500

    Updates for tests

--- spec/jekyll/recipe/parser_spec.rb
+++ spec/jekyll/recipe/parser_spec.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 RSpec.describe Jekyll::Recipe::Parser do
+
   it "has a version number" do
     expect(Jekyll::Recipe::Parser::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+
 end
