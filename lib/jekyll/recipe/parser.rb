bd8ce322c7493d74cc6906ce4a87442a73d9cc50
Wayne Graham <wgraham@clir.org>
2018-02-10 14:51:20 -0500

    Initial config

---
+++ lib/jekyll/recipe/parser.rb
@@ -0,0 +1,9 @@
+require "jekyll/recipe/parser/version"
+
+module Jekyll
+  module Recipe
+    module Parser
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jekyll/recipe/parser/version.rb
@@ -0,0 +1,7 @@
+module Jekyll
+  module Recipe
+    module Parser
+      VERSION = "0.1.0"
+    end
+  end
+end
